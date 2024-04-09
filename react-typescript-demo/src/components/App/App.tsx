125667ac8b777df376b706cbd91386bcc58b4d0a
Jacob Larson <hockeybobber@comcast.net>
2024-04-09 15:56:55 -0500

    added axios template to the project

--- react-typescript-demo/src/components/App/App.tsx
+++ react-typescript-demo/src/components/App/App.tsx
@@ -1,11 +1,47 @@
 import './App.css';
 import Nav from '../Nav/Nav';
+import axios from "axios";
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  useRef,
+  forwardRef,
+} from "react";
 
 function App() {
+
+  useEffect(() => {
+    getAirlines();
+  }, []);
+
+  const [airlines, setAirlines] = useState([{id: 0, airline: "Delta", cost: 1000, }]);
+
+  const getAirlines = () => {
+    axios({
+      method: "GET",
+      url: `/api/characters/basic`,
+    })
+      .then((response) => {
+        setAirlines(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
  
       <h1>Hello World</h1>
+
+      {airlines && airlines.map((airline) => {
+              return (
+                <div key={airline.id}>
+                  <p>{airline.airline} costs ${airline.cost}</p>
+                </div>
+              );
+            })}
 
       <Nav />
 
